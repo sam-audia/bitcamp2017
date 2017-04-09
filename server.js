c544310bf0b2af0fb1388f1e020f4f2caa6c8157
sam-audia <sjaudia@terpmail.umd.edu>
2017-04-09 04:39:03 -0400

    Basic Dashboard Functions and Logout
    needs subtasks and goals

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectId;
 var config = require('./webpack.config');
 
 var app = express();
@@ -33,11 +34,31 @@ app.get('/login', function(req, res) {
 	res.sendFile(path.join(__dirname, 'app/signup.html'));
 })
 
+app.post('/auth', function(req, res) {
+	console.log("hey");
+	console.log(req.body.email);
+	db.collection('users').findOne({"email": req.body.email}, (err, doc) => {
+		if(req.body.password === doc.password)
+		{
+			res.cookie('uid', doc._id);
+			res.redirect('/app/user/' + doc._id);
+		} else {
+			res.redirect('/');
+		}
+	})
+
+});
+
 app.get('/users/goals', function(req, res) {
 	db.collection('goals').find({"uid": req.cookies.uid}).toArray((err, docs) => {
 		res.send(JSON.stringify(docs));
 	})
 
+})
+
+app.post('/logout', function(req, res) {
+	res.clearCookie('uid');
+	res.redirect('/');
 })
 
 app.post('/users/goals', function(req, res) {
@@ -46,6 +67,16 @@ app.post('/users/goals', function(req, res) {
 		if (err) return console.log(err);
 
 		res.redirect('/app/user/' + req.body._id);
+	})
+})
+
+app.delete('/users/goals/:gid', function(req, res) {
+	db.collection('goals').deleteOne({"_id": ObjectId(req.params.gid)});
+})
+
+app.get('/users/info', function(req, res) {
+	db.collection('users').find({"_id": ObjectId(req.cookies.uid)}).toArray((err, docs) => {
+		res.send({"first": docs[0].firstname, "last": docs[0].lastname});
 	})
 })
 
